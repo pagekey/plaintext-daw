b50404187143fe71e6e29acb8d7d928a0830f955
Stephen Grice <stephengrice@live.com>
2023-01-30 18:59:46 -0500

    Add start_beat to Note

--- plaintext_daw/models/note.py
+++ plaintext_daw/models/note.py
@@ -3,14 +3,17 @@ class Note:
     def __init__(
         self,
         value: str = '',
+        start_beat: float = 0,
         length: float = 1,
     ):
         self.value = value
+        self.start_beat = start_beat
         self.length = length
 
     @staticmethod
     def from_dict(dict):
         return Note(
             value=dict['value'] if 'value' in dict else None,
+            start_beat=dict['start_beat'] if 'start_beat' in dict else None,
             length=dict['length'] if 'length' in dict else None,
         )
--- test/models/test_note.py
+++ test/models/test_note.py
@@ -10,7 +10,9 @@ class TestNote:
     def test_from_dict(self):
         obj = Note.from_dict({
             'value': 'a',
+            'start_beat': 5,
             'length': 0.5,
         })
         assert obj.value == 'a'
+        assert obj.start_beat == 5
         assert obj.length == 0.5
