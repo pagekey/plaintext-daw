10e27274c55cae4b022369c0068c8a7d2a058e61
Stephen Grice <stephengrice@live.com>
2023-01-30 19:11:17 -0500

    Add tests for start, end sample on Note

--- plaintext_daw/models/note.py
+++ plaintext_daw/models/note.py
@@ -17,3 +17,14 @@ class Note:
             start_beat=dict['start_beat'] if 'start_beat' in dict else None,
             length=dict['length'] if 'length' in dict else None,
         )
+
+    def get_start_sample(self, sample_rate, bpm):
+        # sample_rate: samples per second
+        # bpm: beats per minute
+        bps = bpm / 60
+        return sample_rate * bps * self.start_beat
+
+    def get_end_sample(self, sample_rate, bpm):
+        bps = bpm / 60
+        len_sample = bps * sample_rate * self.length
+        return self.get_start_sample(sample_rate, bpm) + len_sample
--- test/models/test_note.py
+++ test/models/test_note.py
@@ -16,3 +16,21 @@ class TestNote:
         assert obj.value == 'a'
         assert obj.start_beat == 5
         assert obj.length == 0.5
+
+    def test_start_end_sample_simple(self):
+        note = Note(value='a', start_beat=0, length=1)
+        # regardless of anything else, if it starts on beat 0, it starts on sample 0
+        assert note.get_start_sample(44100, 60) == 0
+        assert note.get_end_sample(44100, 60) == 44100
+
+    def test_start_end_sample_complex(self):
+        note = Note(value='a', start_beat=0.5, length=2)
+        # regardless of anything else, if it starts on beat 0, it starts on sample 0
+        assert note.get_start_sample(44100, 120) == (120/60)*44100*0.5
+        assert note.get_end_sample(44100, 120) == (120/60)*44100*2.5
+
+        assert note.get_start_sample(48000, 120) == (120/60)*48000*0.5
+        assert note.get_end_sample(48000, 120) == (120/60)*48000*2.5
+        note.length = 3
+        assert note.get_start_sample(48000, 120) == (120/60)*48000*0.5
+        assert note.get_end_sample(48000, 120) == (120/60)*48000*3.5
