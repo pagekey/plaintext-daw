dc9ad7838985773f087da9142d64427267b1949c
LTstrange <835422774@qq.com>
2023-02-01 19:35:22 +0800

    Add comments.

--- instrument/effects.py
+++ instrument/effects.py
@@ -7,6 +7,8 @@ import numpy as np
 
 
 def fade_in_out(note: np.ndarray, in_rate=0.1, out_rate=0.9):
+    # This fade_in_out function sound not good.
+    # Will be modified soon.
     assert type(note) == np.ndarray
     note_length = len(note)
 
@@ -22,15 +24,19 @@ def fade_in_out(note: np.ndarray, in_rate=0.1, out_rate=0.9):
 
 
 def sin(base_freq: float, harmonic: int, duration: float, sample_rate: int = 44100):
+    # set up basic parameters
     base_freq = float(base_freq)
     harmonic = int(harmonic)
     duration = float(duration)
     sample_rate = int(sample_rate)
 
+    # set up time
     t = np.arange(0, duration, 1 / sample_rate)
-    amp_sum = 0
-    note_wave = np.zeros_like(t)
+    amp_sum = 0  # record amplitude's sum
+    note_wave = np.zeros_like(t)  # init np array
     for i in range(0, harmonic):
+        # amp:  1 / (2 ** i), i= 0, 1, 2, ...
+        # freq: (2 ** i) * base_freq, i= 0, 1, 2, ...
         amp_sum += 1 / (2 ** i)
         note_wave += 1 / (2 ** i) * np.sin(2 ** i * base_freq * 2 * np.pi * t)
 
--- instrument/models.py
+++ instrument/models.py
@@ -5,8 +5,10 @@
 from dataclasses import dataclass
 from typing import Dict, List, Tuple
 import re
+
+import numpy as np
 import yaml
-from effects import sin, fade_in_out
+from .effects import sin, fade_in_out
 
 
 @dataclass
@@ -16,9 +18,11 @@ class Note:
 
 
 class Instrument:
-    name: str  # instrument's name
-    effects: Dict[str, Tuple[List[str], List[str]]]  # effect_name, list of formal_params, seq of effects
-    notes: Dict[str, Note]  # defined notes
+    def __init__(self):
+        self.name: str = None  # instrument's name
+        self.effects: Dict[
+            str, Tuple[List[str], List[str]]] = dict()  # effect_name, list of formal_params, seq of effects
+        self.notes: Dict[str, Note] = dict()  # defined notes
 
     @staticmethod
     def read_yaml(filename: str):
@@ -28,7 +32,7 @@ class Instrument:
         config = config['instrument']
         instrument = Instrument()
         instrument.set_name(config['name'])
-        instrument.set_effects(config['effects'])
+        instrument.set_effects(config['effects'] if 'effects' in config else None)
         # load notes
         instrument.set_notes(config['notes'])
 
@@ -38,7 +42,9 @@ class Instrument:
         self.name = name
 
     def set_effects(self, effects):
-        self.effects = dict()
+        if effects is None:  # there can be no addition effects. (Only builtin effects)
+            return
+
         for effect_key, effect_seq in effects.items():  # for each effect in config file
             # separate out the name of effect and needed parameters
             effect_name, param = re.match(r'([a-zA-Z]+)\((.*)\)', effect_key).groups()
@@ -47,33 +53,34 @@ class Instrument:
             effect_seq = [e.strip() for e in effect_seq.split('|')]
             self.effects[effect_name] = (param, effect_seq)
 
-    def set_notes(self, notes: Dict[str, str]) -> Note:
-        self.notes = dict()
-        for note_key, note_def in notes.items():
+    def set_notes(self, notes: Dict[str, str]):
+        for note_key, note_def in notes.items():  # for each key definition
+            # separate out the effect's name this note refer, and the value this note give
             effect_name, values = re.match(r'([a-zA-Z]+)\((.*)\)', note_def).groups()
             values = [v.strip() for v in values.split(',')]
-            assert effect_name in self.effects
+            # make sure the effect name is already defined
+            assert effect_name in set(self.effects.keys()).update(['sin', "fade_in_out"])
             params, effect_seq = self.effects[effect_name]
-            assert len(params) == len(values)
+            assert len(params) == len(values)  # make sure the actual para and formal param matched there length
             self.notes[note_key] = Note(values, effect_name)
 
-    def render_note(self, note_name: str, duration: float):
-        note = self.notes[note_name]
-        formal_param, effect_seq = self.effects[note.effect]
+    def render_note(self, note_name: str, duration: float) -> np.ndarray:
+        note = self.notes[note_name]  # get note data
+        formal_param, effect_seq = self.effects[note.effect]  # get effect data
 
-        name_space = dict(zip(formal_param, note.params))
+        name_space = dict(zip(formal_param, note.params))  # set a simple namespace
 
         result = None
-        for effect in effect_seq:
-            name, param = re.match(r'([a-zA-Z_]+)\((.*)\)', effect).groups()
+        for effect in effect_seq:  # apply each effect to generate np array
+            name, param = re.match(r'([a-zA-Z_]+)\((.*)\)', effect).groups()  # separate out effect's name and params
             param = [p.strip() for p in param.split(',')]
-            param = [name_space[p] if p in name_space else p for p in param]
-            if name == "sin":
+            param = [name_space[p] if p in name_space else p for p in param]  # get param's value
+            if name == "sin":  # apply effect
                 param.append(duration)
                 result = sin(*param)
             elif name == 'fade_in_out':
                 assert result is not None
                 result = fade_in_out(result)
             else:
-                raise
+                raise "Not defined effect"
         return result
