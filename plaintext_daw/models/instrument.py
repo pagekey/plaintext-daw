f9111a2a5348091697d24028c71c78f6493e565b
Stephen Grice <stephengrice@live.com>
2023-02-01 21:09:47 -0500

    Add first pass at Git repo loader

--- plaintext_daw/models/instrument.py
+++ plaintext_daw/models/instrument.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import uuid
 from enum import Enum
 from typing import Dict
 
@@ -14,15 +17,57 @@ class Instrument:
     
     def __init__(
         self,
+        name: str = str(uuid.uuid4()),
+        repo: str = '',
+        ref: str = '',
+        path: str = '',
         source: InstrumentSource=InstrumentSource.IN_PLACE,
-        clips: Dict[str, Clip]={}
+        clips: Dict[str, Clip]={},
     ):
+        self.name = name
+        self.repo = repo
+        self.ref = ref
+        self.path = path
         self.source = source
         self.clips = clips
 
     @staticmethod
     def from_dict(dict):
         return Instrument(
+            name=dict['name'] if 'name' in dict else None,
+            repo=dict['repo'] if 'repo' in dict else None,
+            ref=dict['ref'] if 'ref' in dict else None,
+            path=dict['path'] if 'path' in dict else None,
             source=InstrumentSource[dict['source']] if 'source' in dict else None,
             clips={key: Clip.from_dict(elem) for key, elem in dict['clips'].items()} if 'clips' in dict else None,
         )
+
+    def get_repo_name(self):
+        return self.repo.split('/')[-1].replace('.git', '')
+
+    def is_loaded(self):
+        if self.source == InstrumentSource.IN_PLACE:
+            return True # nothing to do
+        elif self.source == InstrumentSource.GIT:
+            return os.path.exists(f'.ptd_cache/instruments/{self.get_repo_name()}')
+        else:
+            return False
+
+    def load(self):
+        if self.source == InstrumentSource.IN_PLACE:
+            pass # nothing to do
+        elif self.source == InstrumentSource.GIT:
+            # Clone the git repo locally
+            # Create cache dir relative to project file if not exists
+            os.makedirs('.ptd_cache/instruments', exist_ok=True)
+            orig_pwd = os.getcwd()
+            os.chdir('.ptd_cache/instruments')
+            repo_name = self.get_repo_name()
+            if not os.path.exists(repo_name):
+                subprocess.check_call(f'git clone {self.repo}'.split())
+            os.chdir(repo_name)
+            if self.ref is not None and len(self.ref) > 0:
+                subprocess.check_call(f'git checkout {self.ref}'.split())
+            os.chdir(orig_pwd)
+        else:
+            raise ValueError('Load instrument source not yet supported:', str(self.source))
