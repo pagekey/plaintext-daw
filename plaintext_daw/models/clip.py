a18503a66934b35dcc2bf590af64ce7830153f54
Stephen Grice <stephengrice@live.com>
2023-02-01 22:18:29 -0500

    Add clip type

--- plaintext_daw/models/clip.py
+++ plaintext_daw/models/clip.py
@@ -1,13 +1,19 @@
+from enum import Enum
 import os
 import numpy as np
 
 from ..lib import wav_to_np
 
 
+class ClipType(Enum):
+    WAV = 0
+    SINE = 1
+
 class Clip:
 
     def __init__(
         self,
+        type: ClipType = ClipType.WAV,
         path: str = '',
         start: int = 0,
         config_dir: str = '.',
@@ -19,11 +25,17 @@ class Clip:
     @staticmethod
     def from_dict(dict, config_dir):
         return Clip(
+            type=ClipType[dict['type']] if 'type' in dict else None,
             path=dict['path'] if 'path' in dict else None,
             start=dict['start'] if 'start' in dict else None,
             config_dir=config_dir,
         )
 
     def to_np(self):
-        wav_path = os.path.join(self.config_dir, self.path)
-        return wav_to_np(wav_path)
+        if self.type == ClipType.WAV:
+            wav_path = os.path.join(self.config_dir, self.path)
+            return wav_to_np(wav_path)
+        elif self.type == ClipType.SINE:
+            pass
+        else:
+            raise ValueError('ClipType not yet supported:', str(self.type))
