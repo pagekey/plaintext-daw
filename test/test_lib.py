684cce5dcf552e61c62b5c1ce17aeb6bff8e8f5c
LTstrange <835422774@qq.com>
2023-02-07 10:13:15 +0800

    fix issue #8

--- test/test_lib.py
+++ test/test_lib.py
@@ -1,5 +1,5 @@
 from plaintext_daw.lib import np_to_mp3
-from plaintext_daw.models.synth import sin
+from plaintext_daw.models.synth import gen_sine
 import os
 
 
@@ -7,7 +7,7 @@ def test_convert_mp3():
     if os.path.exists("song.mp3"): os.remove("song.mp3")
 
     sample_rate = 44100
-    signal = sin(65.406, 5, 2, sample_rate)
+    signal = gen_sine(65.406, 5, 2, sample_rate)
     np_to_mp3(signal, sample_rate, "song.mp3")
 
     assert os.path.exists("song.mp3")
