58cbf18245dca5134a827d8b4b58653de5885d21
LTstrange <835422774@qq.com>
2023-02-15 17:31:21 +0800

    add test of min_max_mapping

--- test/test_lib.py
+++ test/test_lib.py
@@ -1,4 +1,4 @@
-from plaintext_daw.lib import np_to_mp3
+from plaintext_daw.lib import np_to_mp3, minmax_mapping
 from plaintext_daw.models.synthesizer.envelope import ADSR
 from plaintext_daw.models.synthesizer.rawclip import RawClip
 
@@ -6,6 +6,12 @@ import os
 
 from plaintext_daw.models.synthesizer.wave import Wave, Sine
 
+def test_min_max_mapping():
+    sample_rate = 44100
+    signal = RawClip(Wave([Sine(65.406, 2)]), ADSR(0.1, 0, 1, 0.1), duration=2).render(sample_rate)
+    assert signal.max() > 1 and signal.min() < -1
+    signal = minmax_mapping(signal)
+    assert signal.max() < 1 and signal.min() > -1
 
 def test_convert_mp3():
     if os.path.exists("song.mp3"): os.remove("song.mp3")
