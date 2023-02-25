06cb759a1385e73abe17328cd944a8fff8c390fe
Stephen Grice <stephengrice@live.com>
2023-02-25 16:15:56 -0500

    Delete un-needed stuff in GUI, shrink window

--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -7,22 +7,6 @@ use std::process::Command;
 use tauri_api::dialog;
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn syscall_test() -> () {
-    println!("hello tauri");
-    Command::new("ls")
-        .arg("-l")
-        .arg("-a")
-        .spawn()
-        .expect("ls command failed to start");
-    ()
-}
-
-#[tauri::command]
 fn open_project() -> () {
     println!("Opening!");
     match dialog::select(Some(""), Some("")) {
@@ -47,25 +31,13 @@ fn open_project() -> () {
             println!("Success file open")
         }
         Err(_) => {
-            println!("Open file faild")
+            println!("Open file failed")
         }
     }
 }
 
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b;
-}
-
-#[test]
-fn test_add() {
-    let result = add(2, 3);
-    assert_eq!(result, 5);
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![syscall_test])
         .invoke_handler(tauri::generate_handler![open_project])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- tauri/src/main.js
+++ tauri/src/main.js
@@ -1,13 +1,5 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
 async function syscall() {
   await invoke('syscall_test', {});
 }
@@ -16,14 +8,6 @@ async function open_project() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-  document
-    .querySelector("#syscall-button")
-    .addEventListener("click", () => syscall());
   document
     .querySelector("#open-project")
     .addEventListener("click", () => open_project());
