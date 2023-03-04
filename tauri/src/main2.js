c9cff598134ab2c1a417fc1c3a2a71c297967d82
Stephen Grice <stephengrice@live.com>
2023-03-03 21:49:17 -0500

    Fill in more values

--- tauri/src/main2.js
+++ tauri/src/main2.js
@@ -3,8 +3,9 @@ const { invoke } = window.__TAURI__.tauri;
 
 window.addEventListener("DOMContentLoaded", async () => {
     invoke('get_app').then((app) => {
+        document.querySelector("#filepath-value").innerHTML = app.filepath;
         document.querySelector("#bpm-value").innerHTML = app.song.bpm;
-        document.querySelector("#sample-rate-value").innerHTML = app.song.bpm;
+        document.querySelector("#sample-rate-value").innerHTML = app.song.sample_rate;
         document
             .querySelector("#current_project").innerHTML = `filepath: ${app.filepath}\n\n\ncontents:: ${app.contents}`;
     });
